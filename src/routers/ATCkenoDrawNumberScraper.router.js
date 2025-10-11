042178c668873ec54b6c425f9e6e4e94fe820197
Kajal Gurjar <dev29.mxpertz@gmail.com>
2025-10-11 16:51:27 +0530

    add the VIC module

---
+++ src/routers/ATCkenoDrawNumberScraper.router.js
@@ -0,0 +1,43 @@
+import express from "express";
+import {
+  scrapeATCKeno,
+  // scrapeNSWKenobyGame,
+  // getKenoResults,
+  // getFilteredKenoResults,
+} from "../controllers/kenoScraper/ATCkenoDrawNumberScraper.controller.js";
+import verifyAPIKey from "../middleware/verifyAPIKey.js";
+
+const router = express.Router();
+
+// router.get("/nsw", scrapeAndParseNSWKeno);
+router.get("/atc-latest", async (req, res) => {
+  try {
+    const data = await scrapeATCKeno();
+    res.status(200).json({
+      success: true,
+      message: "VIC Keno results scraped successfully",
+      data,
+    });
+  } catch (error) {
+    console.error("Scraping error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to scrape VIC Keno results",
+      error: error.message,
+    });
+  }
+});
+
+// router.get("/latestbyGame", async (req, res) => {
+//   try {
+//     const results = await scrapeNSWKenobyGame();
+//     res.json(results);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
+
+// router.get("/keno-results", verifyAPIKey, getKenoResults);
+// router.get("/applyfilters", verifyAPIKey, getFilteredKenoResults);
+
+export default router;
