1bc3ad669eb22c25a6430b43f35ed91840c285eb
Kajal Gurjar <dev29.mxpertz@gmail.com>
2025-11-07 11:27:40 +0530

    add the over due api

---
+++ src/routers/OverdueCombos.router.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getOverdueCombos,
+  getOverdueCombospagination,
+} from "../controllers/ExoticPredictor/overdueCombo.controller.js";
+
+const router = express.Router();
+
+// Analyze all locations and generate overdue combos
+router.get("/generate", getOverdueCombos);
+router.get("/generate/pagination", getOverdueCombospagination);
+
+export default router;
+
+
+
