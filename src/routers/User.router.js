3fc808610e12db55169795d212946222b7176c2d
Kajal Gurjar <dev29.mxpertz@gmail.com>
2026-01-13 11:27:14 +0530

    add the data with remove api-key

--- src/routers/User.router.js
+++ src/routers/User.router.js
@@ -4,17 +4,17 @@ import {
   verifyOtp,
   setPassword,
   loginUser,
-  saveFcmToken
+  saveFcmToken,
 } from "../controllers/Authentication/UserRegister.controller.js";
 import verifyAPIKey from "../middleware/verifyAPIKey.js";
 import auth from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/register", verifyAPIKey, registerUser);
-router.post("/verify-otp", verifyAPIKey, verifyOtp);
-router.post("/set-password", verifyAPIKey, setPassword);
-router.post("/login", verifyAPIKey, loginUser);
+router.post("/register", registerUser);
+router.post("/verify-otp", verifyOtp);
+router.post("/set-password", setPassword);
+router.post("/login", loginUser);
 
 // 🔔 FIXED ROUTE (VERY IMPORTANT)
 router.post("/save-fcm-token", auth, saveFcmToken);
