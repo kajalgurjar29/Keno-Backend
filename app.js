11860a5b2e0a705aeb04bd6f5f9b95acf8ceff01
Kajal Gurjar <dev29.mxpertz@gmail.com>
2026-01-17 18:34:30 +0530

    add api-key

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "api-key"],
   }),
 );
 app.options("*", cors());
