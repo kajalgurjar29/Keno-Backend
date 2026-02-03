5e556cc86184bf33af8d9eee5410fdac57c29707
Kajal Gurjar <dev29.mxpertz@gmail.com>
2026-02-03 17:33:17 +0530

    remove checksbcription

--- src/routers/OverdueCombos.router.js
+++ src/routers/OverdueCombos.router.js
@@ -7,18 +7,16 @@ import {
   getTracksideOverdue,
   getTracksideOverduePagination,
 } from "../controllers/ExoticPredictor/tracksideOverdue.controller.js";
-
 import auth from "../middleware/auth.middleware.js";
-import { checkSubscription } from "../middleware/subscription.middleware.js";
 
 const router = express.Router();
 
 // Analyze all locations and generate overdue combos
-router.get("/generate/", auth, checkSubscription, getOverdueCombos);
-router.get("/generate/pagination", auth, checkSubscription, getOverdueCombospagination);
+router.get("/generate/", auth, getOverdueCombos);
+router.get("/generate/pagination", auth, getOverdueCombospagination);
 
 // Analyze Trackside locations and generate overdue combos
-router.get("/trackside/generate/", auth, checkSubscription, getTracksideOverdue);
-router.get("/trackside/generate/pagination", auth, checkSubscription, getTracksideOverduePagination);
+router.get("/trackside/generate/", auth, getTracksideOverdue);
+router.get("/trackside/generate/pagination", auth, getTracksideOverduePagination);
 
 export default router;
--- src/routers/TracksideAnalytics.router.js
+++ src/routers/TracksideAnalytics.router.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { getTop10Exotics, getTracksideHorseEntryDetails } from "../controllers/TracksideAnalytics/TracksideAnalytics.controller.js";
 import auth from "../middleware/auth.middleware.js";
-import { checkSubscription } from "../middleware/subscription.middleware.js";
 
 const router = express.Router();
 
-router.get("/top-10", auth, checkSubscription, getTop10Exotics);
-router.get("/horse-details/:horseNo", auth, checkSubscription, getTracksideHorseEntryDetails);
+router.get("/top-10", auth, getTop10Exotics);
+router.get("/horse-details/:horseNo", auth, getTracksideHorseEntryDetails);
 
 export default router;
