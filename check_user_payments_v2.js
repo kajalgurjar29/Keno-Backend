1df3d5bdaa5ae11fba6ff68c14376a8bfbb52b9f
Kajal Gurjar <dev29.mxpertz@gmail.com>
2026-03-13 17:22:48 +0530

    add new webhook

---
+++ check_user_payments_v2.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import User from './src/models/User.model.js';
+import Payment from './src/models/Payment.js';
+
+dotenv.config();
+
+async function checkUserStats() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        const userId = "69a97e0c65aa0df002996396";
+
+        const user = await User.findById(userId);
+        console.log("--- User Record ---");
+        console.log(JSON.stringify({email: user.email, isSubscriptionActive: user.isSubscriptionActive, isSubscribed: user.isSubscribed, planType: user.planType, subscriptionEnd: user.subscriptionEnd}, null, 2));
+
+        const payments = await Payment.find({ userId }).sort({ createdAt: -1 });
+        console.log("\n--- Payment Records ---");
+        console.log(JSON.stringify(payments, null, 2));
+
+        process.exit(0);
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+checkUserStats();
